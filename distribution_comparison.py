4883cbf1d15af3db0b9fbf54958a4f272a2b6381
serna-r <roisanch@pa.uc3m.es>
2024-09-10 20:46:47 +0200

    addition of distribution comparison per length

--- distribution_comparison.py
+++ distribution_comparison.py
@@ -84,6 +84,20 @@ def get_count_and_probabilities(data):
     # Return probability list
     return count_list, probability_dist
 
+def get_score_and_length(file_path):
+    # Read the file into a DataFrame
+    df = pd.read_csv(file_path, sep='\s+', skiprows=1)
+
+    # Rename the 'Length' column from the index to make it a column
+    df.rename(columns={'score': 'length'}, inplace=True)
+    # Drop the first row (which contains 'Length Group' etc.)
+    df = df.drop(index=0).reset_index(drop=True)
+
+    # Reset index so "Length" is no longer part of the index
+    df.reset_index(drop=True, inplace=True)
+    # Return the obtained df
+    return df
+
 def compute_kl_matrix(distributions, names):
     num_distributions = len(distributions)
     kl_matrix = np.zeros((num_distributions, num_distributions))
@@ -91,8 +105,9 @@ def compute_kl_matrix(distributions, names):
     for i in range(num_distributions):
         for j in range(num_distributions):
             if i != j:
+                # print(distributions[i], distributions[j], '\n')
                 kl_matrix[i, j] = entropy(distributions[i], distributions[j], nan_policy='omit')
-                if math.isinf(kl_matrix[i, j]): print(distributions[i], distributions[j])
+                # if math.isinf(kl_matrix[i, j]): print(distributions[i], distributions[j])
             else:
                 kl_matrix[i, j] = 0.0  # KL divergence between identical distributions is 0
 
@@ -101,22 +116,25 @@ def compute_kl_matrix(distributions, names):
 
     return kl_df
 
-def compute_kl_matrix_mask(distributions, names):
-    # Eliminate mask column
-    distributions_drop_mask = [df.drop(['mask'], axis=1) for df in distributions]
+def compute_kl_matrix_dfs(distributions, names, dropcolumn, forceformat='float'):
+    # Eliminate column without data
+    distributions_drop_column = [df.drop([dropcolumn], axis=1) for df in distributions]
 
     kl_matrices = []
     # For each length distribution calculate kl matrix
-    for i in range(len(distributions_drop_mask[0].index)):
+    for i in range(len(distributions_drop_column[0].index)):
         # Get each length distribution
         length_dist = []
-        for distribution in distributions_drop_mask:
-            length_dist.append(distribution.iloc[i].to_list())
+        for distribution in distributions_drop_column:
+            if forceformat == 'none' : length_dist.append(distribution.iloc[i].to_list())
+            # Check if there is a need to format the elements to float
+            elif forceformat == 'float': length_dist.append(list(map(float, distribution.iloc[i].to_list())))
         
         # Append length and kl matrix
         kl_matrices.append([(i+5),compute_kl_matrix(length_dist, names)])
 
     return kl_matrices
+
 
 def plot_distributions(distributions, names):
     num_distributions = len(distributions)
@@ -138,10 +156,11 @@ def plot_distributions(distributions, names):
     plt.tight_layout()
     return plt
 
-def plot_matrix(data, labels, cmap):
+def plot_matrix(data, labels, cmap, vmin=0, vmax=2):
     # Create plot
     fig, ax = plt.subplots()
-    cax = ax.matshow(data, cmap=cmap)
+    # Set color scale to range from 0 to 2
+    cax = ax.matshow(data, cmap=cmap, vmin=vmin, vmax=vmax)
 
     # Add color bar
     plt.colorbar(cax)
@@ -174,13 +193,14 @@ def get_distribution_comparison():
     counts = []
     score_distributions = []
     mask_dataframes = []
+    score_length_dataframes = []
     
     # For each leak get score distributions
     for leak in leak_names:
         # Get leak file name
-        leak = './' + leak + '/Stats.txt'
+        leak_file = './' + leak + '/Stats.txt'
         # Open file and get stats
-        with open(leak, 'r') as f:
+        with open(leak_file, 'r') as f:
             # Read file
             data = f.read()
             # Get count and probabilities for score
@@ -188,17 +208,25 @@ def get_distribution_comparison():
             # Get mask distributions
             mask_df = get_mask_distribution(data)
 
-        # Get score counts, probabilities and scores
+        # Define the path to the txt file
+        leak_scores_and_length_file = leak + '\password_score_and_length.txt'
+        score_length_df = get_score_and_length(leak_scores_and_length_file)
+
+        # Get score counts, probabilities, scores and scores by length
         counts.append(count)
         score_distributions.append(probability)
         mask_dataframes.append(mask_df)
+        score_length_dataframes.append(score_length_df)
     
     
     # Get the kl matrix for score
     kl_df_score = compute_kl_matrix(score_distributions, leak_names)
 
     # Get kl matrix for mask and length
-    kl_dfs_mask = compute_kl_matrix_mask(mask_dataframes, leak_names)
+    kl_dfs_mask = compute_kl_matrix_dfs(mask_dataframes, leak_names, 'mask')
+
+    # Get kl matrix for score and length, format np to eliminate np.float values
+    kl_dfs_score_length = compute_kl_matrix_dfs(score_length_dataframes, leak_names, 'length', forceformat='float')
 
     # Plot and save the score distributions
     plot_distributions(score_distributions, leak_names).savefig(figures_folder + 'scores_distribution.png')
@@ -208,6 +236,10 @@ def get_distribution_comparison():
     # For each mask matrix plot
     for item in kl_dfs_mask:
         plot_matrix(item[1].values, leak_names, 'coolwarm').savefig(figures_folder + f'mask_length_{item[0]}_kl_matrix.png')
+
+    # For each mask matrix plot
+    for item in kl_dfs_score_length:
+        plot_matrix(item[1].values, leak_names, 'coolwarm').savefig(figures_folder + f'score_length_{item[0]}_kl_matrix.png')
 
 if __name__ == '__main__':
     get_distribution_comparison()
