6bca58bd52154c0e0c610b23acb808926f76d692
serna-r <100405914@alumnos.uc3m.es>
2024-09-12 14:17:22 +0200

    shein update

--- tests.py
+++ tests.py
@@ -1,22 +1,33 @@
 import pandas as pd
+import re
 
-# Sample data
-data = {
-    'Platform': ['LinkedIn', 'Canva'],
-    'Value 0': [3958629.0, 73021.0],
-    'Value 1': [14259464.0, 262482.0]
-}
+def split_email_and_password(string):
+    # Regular expression to capture email and password
+    pattern = r"([^:]+)@([^:]+):(.+)"
+    
+    # Search for matches
+    match = re.match(pattern, string)
+    
+    if match:
+        email = match.group(1) + "@" + match.group(2)
+        password = match.group(3)
+        return email, password
+    else:
+        return None, None
 
-# Create DataFrame
-df = pd.DataFrame(data)
+data = []
+verbose = 2
+file_path = './shein/data/shein.com 30kk.txt'
+with open(file_path, 'r', encoding='utf-8') as f:
+    for line in f:
+        email, password = split_email_and_password(line)
+        if email is not None and password is not None:
+            # Check also for unknown or null passwords
+            if (password == 'NULL' or password == 'none' or password == '?' 
+                    or password == 'None'):
+                continue
+            data.append([email, password])
+            if verbose > 1: print(f"email: {email} password: {password}")
 
-# Set the 'Platform' column as index (if you want to keep it)
-df.set_index('Platform', inplace=True)
-
-# Divide each row by the sum of the row
-df_normalized = df.div(df.sum(axis=1), axis=0)
-
-print("Original DataFrame:")
-print(df)
-print("\nNormalized DataFrame:")
-print(df_normalized)
+# Create a pandas DataFrame with all the collected data
+df = pd.DataFrame(data, columns=['User', 'Password'])
