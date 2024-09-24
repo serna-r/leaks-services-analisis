8c4fc7d08c0b4dc25339c8aef8e21555e3d11c23
serna-r <roisanch@pa.uc3m.es>
2024-09-24 17:03:45 +0200

    latex reorder and edit

--- write_latex.py
+++ write_latex.py
@@ -4,15 +4,15 @@ def write_latex_table(data, output_path):
     with open(output_path, 'w') as file:
         file.write("\\begin{table*}[h!]\n")
         file.write("\\centering\n")
-        file.write("\\begin{tabular}{|l|c|c|c|c|}\n") 
+        file.write("\\begin{tabular}{|l|l|c|c|c|c|}\n")  # Added a new 'l' for the Category column
         file.write("\\hline\n")
-        file.write("\\textbf{Source} & \\textbf{Date} & \\textbf{Number of Users} & \\textbf{Mean Length} & \\textbf{Mean Score} \\\\\n")
+        file.write("\\textbf{Source} & \\textbf{Category} & \\textbf{Date} & \\textbf{Number of Users} & \\textbf{Mean Length} & \\textbf{Mean Score} \\\\\n")
         file.write("\\hline\n")
         # Loop through the data dictionary and fill the table with appropriate values
         for source, stats in data.items():
-            num_users, mean_length, mean_score = stats
+            category, num_users, mean_length, mean_score  = stats  # Unpack the category
             # The Date column is left empty as per your table structure
-            file.write(f"{source} &  & {num_users} & {mean_length:.2f} & {mean_score:.2f} \\\\\n")
+            file.write(f"{source} & {category} &  & {num_users} & {mean_length:.2f} & {mean_score:.2f} \\\\\n")
         file.write("\\hline\n")
         file.write("\\end{tabular}\n")
         file.write("\\caption{Summary of data breaches with user information.}\n")
@@ -32,7 +32,7 @@ def get_latex_table(output_path):
     # Dictionary to store the results for each leak
     leak_data = {}
     
-    for leak, _ in leak_types:
+    for leak, category in leak_types:
         # Get leak file name
         leak_file = './leaks/' + leak + '/Stats.txt'
         
@@ -51,7 +51,7 @@ def get_latex_table(output_path):
                 mean_score = calculate_mean_score(probability_dist)
                 
                 # Add to the dictionary (number of users, mean length, mean score)
-                leak_data[leak] = (count, mean_length, mean_score)
+                leak_data[leak] = ( category, count, mean_length, mean_score)
         
         except FileNotFoundError:
             print(f"File not found: {leak_file}")
